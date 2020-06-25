a8fe7bc694b14dcb0ff7a2a7ea23e662466a7489
yazzy4 <yazburrell@pursuit.org>
2020-06-25 14:33:42 -0400

    Working on user defaults for settings

--- UserDefaults/VCs/SettingsTVC.swift
+++ UserDefaults/VCs/SettingsTVC.swift
@@ -22,7 +22,7 @@ class SettingsTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadSettings()
        
     }
 
@@ -32,11 +32,32 @@ class SettingsTVC: UITableViewController {
         coolMeterValueLabel.text = String(Int(sender.value))
     }
     
+    func loadSettings(){
+        
+        guard let settingsDictionary = UserDefaults.standard.dictionary(forKey: "settings") else { return }
+//        let firstName = UserDefaults.standard.string(forKey: "firstName")
+//        print(firstName ?? "no first name")
+    }
+    
+    func saveSettings(){
+        
+        let settingsDictionary: [String:Any?] = ["firstName":firstNameTextField.text,
+             "lastName": lastNameTextField.text,
+             "likesDevelopment": likesDevelopmentSwitch.isOn,
+             "coolMeterValue": Int(coolMeterSlider.value)
+             ]
+          
+        UserDefaults.standard.set(settingsDictionary, forKey: "settings")
+        
+//        UserDefaults.standard.set(firstNameTextField.text, forKey: "firstName")
+    }
+    
  
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard indexPath.section == 2 else { return }
-        
+       
         print("save")
+        saveSettings()
     }
     
 }
